591ae66147ecf055ab4188fbdfa9aaabcd2c9a1d
Kamil T <12988954+Szwagi@users.noreply.github.com>
2023-03-09 15:33:43 +0000

    Fix privilege

--- gokz.ps1
+++ gokz.ps1
@@ -22,6 +22,12 @@ $startMap = ""
 $ErrorActionPreference = "Stop"
 $global:ProgressPreference = "SilentlyContinue"
 
+# You can remove this 'if' block after you've ran it for the first time, so you don't get the UAC prompts every time.
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { 
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; 
+    exit 
+}
+
 if (-Not (Test-Path -Path $steamCmdDir)) {
     New-Item -Path $steamCmdDir -ItemType Directory | Out-Null
 }
